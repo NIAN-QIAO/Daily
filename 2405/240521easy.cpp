51505b500a620448efb6ab408f5f7596bf258c30
WEH <2262793760@qq.com>
2024-05-21 11:37:14 +0800

    240521 easy~

---
+++ 2405/240521easy.cpp
@@ -0,0 +1,9 @@
+//2769. 找出最大的可达成数字
+// Created by PC on 2024/5/21.
+//https://leetcode.cn/problems/find-the-maximum-achievable-number/description/
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+};
