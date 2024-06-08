3508d2fb5a42ae8b4d1204faaa7e9bc1f0ddd377
WEH <2262793760@qq.com>
2024-06-08 16:15:10 +0800

    NO240608medium

---
+++ 2406/NO240608medium.cpp
@@ -0,0 +1,14 @@
+//3040. 相同分数的最大操作数目 II
+// Created by PC on 2024/6/8.
+//https://leetcode.cn/problems/maximum-number-of-operations-with-the-same-score-ii/description/
+#include "deque"
+#include "iostream"
+#include "vector"
+using namespace std;
+class Solution {
+public:
+    int maxOperations(vector<int> &nums) {
+        if (nums.size() < 2)
+            return 0;
+    }
+};
